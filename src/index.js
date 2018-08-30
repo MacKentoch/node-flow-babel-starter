9804211e229d2c24b458cefb6ef1c2862a66b2df
MacKentoch <erwan.datin@gmail.com>
2018-08-30 17:42:45 +0200

    add a comment in src example to explain project scope types

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 // #region constants
+// NOTE: flow type "DummyCallBack" comes from <ROOT_PROJECT_DIR>/flow_typed (no need to explicitely import)
 const delayedNothing = (cb: DummyCallBack) =>
   new Promise(resolve => process.nextTick(() => resolve(cb())));
 // #endregion
